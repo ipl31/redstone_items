cc5cc3b1b71d91505890cf3b3df652ac6a130803
ken caruso <ken@ipl31.net>
2023-10-29 12:21:31 -0700

    initial redstone items

---
+++ src/main/java/io/kc2/minecraft/mods/RedStoneItemsDataGenerator.java
@@ -0,0 +1,11 @@
+package io.kc2.minecraft.mods;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RedStoneItemsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
